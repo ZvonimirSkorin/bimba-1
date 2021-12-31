cb76ef7fc21926ae7b96714b7f0e4cc9d9f94d26
unknown <marsel.corluka.travian@gmail.com>
2021-12-31 04:14:43 +0100

    popravci

--- components/About/Section2.tsx
+++ components/About/Section2.tsx
@@ -4,13 +4,14 @@ export const Section2: React.FC = () => {
   return (
     <div className={styles.Wrapper}>
       <section className={styles.ImgWrapper}>
-        <Image alt="" src="/Cat.jpg" layout="fill" height={"100%"} width="100%" objectFit="cover" />
+        <Image alt="" src="/Cat.jpg" layout="fill" objectFit="cover" />
       </section>
+
       <section className={styles.Box}>
         <h2 className={styles.h2}>Što mi radimo</h2>
-        <h1 className={styles.h1}>Popularne usluge</h1>
+        <h1 className={styles.h3}>Popularne usluge</h1>
         <div className={styles.ServiceBox}>
-          <h3 style={{ color: "black" }}>Potpuna njega</h3>
+          <h3 style={{ color: "#333" }}>Potpuna njega</h3>
           <ul>
             {new Array(20).fill(0).map((v) => {
               return <li key={v}>Lorem ipsum sincor venetur</li>;
