4e3bcc28d5f6a81f7d054fcbdfab0455494d3918
unknown <marsel.corluka.travian@gmail.com>
2022-01-05 19:49:13 +0100

    landing

--- components/Index/gallery.tsx
+++ components/Index/gallery.tsx
@@ -1,11 +1,10 @@
 import styles from "./gallery.module.scss"
 import Image from "next/image"
 import Link from "next/link";
+import {LightBoxGalery} from "../SharedComponents/LightBoxGalery";
 
 function Gallery() {
 
-    
-  
     
   
     return(
@@ -15,10 +14,12 @@ function Gallery() {
             <h1 className={styles.h1}>Galerija</h1>
                      {new Array(8).fill(0).map((v) => {
                     return <div className={styles.imgWrap}>
-                                <Image src="/Cat.jpg" alt=""width={200}height={200}/>
+                                <Image  key={v} src="/Cat.jpg" alt=""width={200} height={200}/>
+
                            </div>
+
                     ;})}
-                
+
             </div>
         </div>
         
