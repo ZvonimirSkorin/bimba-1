076be171aa65e3b8cb0921416344c2c2a76672ec
ZvonimirSkorin <80110264+ZvonimirSkorin@users.noreply.github.com>
2022-01-07 19:18:38 +0100

    Update API.tsx

--- components/Services/API.tsx
+++ components/Services/API.tsx
@@ -15,7 +15,7 @@ export const SendMail = (obj: mail, setAlert: Function) => {
     return;
   }
   axios
-    .post("http://localhost:3000/api/SendMail", obj)
+    .post("http://bimba.vercel.app/api/SendMail", obj)
     .then(() => {
       setAlert({ type: 1, message: "Uspješno poslan mail" });
     })
